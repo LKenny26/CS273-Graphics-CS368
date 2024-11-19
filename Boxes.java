ab47122e830b8d85f23f574d04e4f0c37126d464
LKenny26 <kenny26@up.edu>
2024-11-18 17:06:39 -0800

    Fix Boxes
    Drawing the Boxes one at a time.
    Showcases first the outer box, then the middle box, and finally the inner box.
    Utilizes a sleep call to help the user visualize the steps better.
    This is to ensure students understand how each box is drawn on top of each other. This is why if the inner box were to have been called first it would have been covered up.

--- Boxes.java
+++ Boxes.java
@@ -22,13 +22,54 @@ public class Boxes extends Canvas
     {
         //This line is important, don't remove it!
         super.paint(g);
-        
-        //These statements draw some boxes, but they are not the boxes you want.
-        //Replace them with the correct code.
+        Font font = new Font("Serif", Font.PLAIN, 40);
+
+        // Draw the Outer Box
+        g.setFont(font);
+        g.setColor(Color.black);
+        g.drawString("Drawing Outer Box...", 50, 50);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        g.setColor(Color.blue);
+        g.fillRect(200, 200, 500, 500);
+
+        // Draw the middle box
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        g.setFont(font);
+        g.setColor(Color.black);
+        g.drawString("Drawing Middle Box...", 50, 100);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         g.setColor(Color.red);
-        g.fillRect(100,100,100,100);
+        g.fillRect(300,300,300,300);
+
+        // Draw the inner box
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        g.setFont(font);
+        g.setColor(Color.black);
+        g.drawString("Drawing Inner Box...", 50, 150);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         g.setColor(Color.green);
-        g.fillRect(200,200,100,100);
+        g.fillRect(375,375,150,150);
         
     }//paint
 
@@ -45,7 +86,7 @@ public class Boxes extends Canvas
         //Create a window for this program
         JFrame myFrame = new JFrame();
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setSize(600,600);
+        myFrame.setSize(1000,1000);
  
         //Put an instance of this class in the window frame
         Boxes myContent = new Boxes();
